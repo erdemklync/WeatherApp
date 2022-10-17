afc1be21eeaf86839ec9af0a1160ceb45d551133
Erdem Kalyoncu <ekalyoncu81@gmail.com>
2022-10-17 22:57:29 +0300

    Extract strings

--- app/src/main/java/com/ekalyoncu/weatherapp/MainActivity.kt
+++ app/src/main/java/com/ekalyoncu/weatherapp/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.ekalyoncu.weatherapp
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import androidx.core.view.WindowCompat
-import com.ekalyoncu.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
 }
